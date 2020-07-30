17f5c1dd637750426d00fb22745eaae3dc29d824
Jack Cullen <jpcullen20@gmail.com>
2020-07-30 10:50:40 -0600

    Add helper method to remove repitition

--- app/poros/nytimes_results.rb
+++ app/poros/nytimes_results.rb
@@ -3,64 +3,50 @@ require_relative './obituary.rb'
 
 class NytimesResults
   def get_obituaries
-    json = NytimesService.new.get_obituaries
-
-    json[:response][:docs].map do |obituary_data|
-
-      Obituary.new(format_obituary_data(obituary_data))
-    end
+    parsed_json = NytimesService.new.get_obituaries
+    data_to_obituaries(parsed_json)
   end
 
   def get_covid_obituaries
-    json = NytimesService.new.get_covid_obituaries
+    parsed_json = NytimesService.new.get_covid_obituaries
+    data_to_obituaries(parsed_json)
 
-
-    json[:response][:docs].map do |obituary_data|
-
-      Obituary.new(format_obituary_data(obituary_data))
-    end
   end
 
   def get_obituaries_by_name(name)
-    json = NytimesService.new.get_obituaries_by_name(name)
-    json[:response][:docs].map do |obituary_data|
-      Obituary.new(format_obituary_data(obituary_data))
-    end
+    parsed_json = NytimesService.new.get_obituaries_by_name(name)
+    data_to_obituaries(parsed_json)
   end
 
   def get_obituaries_by_date(beginning_date, ending_date)
-    json = NytimesService.new.get_obituaries_by_date(beginning_date, ending_date)
-    json[:response][:docs].map do |obituary_data|
-      Obituary.new(format_obituary_data(obituary_data))
-    end
+    parsed_json = NytimesService.new.get_obituaries_by_date(beginning_date, ending_date)
+    data_to_obituaries(parsed_json)
   end
 
   def get_most_recent_obituaries
-    json = NytimesService.new.get_most_recent_obituaries
-    json[:response][:docs].map do |obituary_data|
-      Obituary.new(format_obituary_data(obituary_data))
-    end
+    parsed_json = NytimesService.new.get_most_recent_obituaries
+    data_to_obituaries(parsed_json)
   end
 
   def get_advanced_obituaries(name, date)
-    json = NytimesService.new.get_advanced_obituaries(name, date)
-    json[:response][:docs].map do |obituary_data|
-      Obituary.new(format_obituary_data(obituary_data))
-    end
+    parsed_json = NytimesService.new.get_advanced_obituaries(name, date)
+    data_to_obituaries(parsed_json)
   end
 
   def get_obituaries_by_year(date)
-    json = NytimesService.new.get_obituaries_by_year(date)
-    json[:response][:docs].map do |obituary_data|
+    parsed_json = NytimesService.new.get_obituaries_by_year(date)
+    data_to_obituaries(parsed_json)
+  end
+
+  private
+
+  def data_to_obituaries(parsed_json)
+    parsed_json[:response][:docs].map do |obituary_data|
       Obituary.new(format_obituary_data(obituary_data))
     end
   end
 
-
-
-  private
   def format_obituary_data(obituary_data)
-
     {
       headline: obituary_data[:headline][:main],
       abstract: obituary_data[:abstract],
